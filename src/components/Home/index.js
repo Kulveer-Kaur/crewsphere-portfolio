11514e6c6070caacaaee629c0eda443938d7ee9a
Kulveer Kaur <kulveerkaurpctebtech20@gmail.com>
2023-05-13 10:17:03 +0530

    Home-Page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,23 +1,64 @@
 import LogoTitle from '../../assets/images/k2.png'
 import { Link } from 'react-router-dom'
 import './index.scss'
+import { useEffect, useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['u', 'l', 'v', 'e', 'e', 'r']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    '',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br />
-          I'm
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
           <img src={LogoTitle} alt="developer" />
-          ulveer
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
           <br />
           <br />
-          web developer
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
         </h1>
         <h2>Frontend developer / Data Scientist / Painter</h2>
         <Link to="/contact" className="flat-button">
           CONTACT ME
         </Link>
+      </div>
+      <Logo />
+      <div className="myimage">
+        <img src={LogoTitle} alt="developer" />
       </div>
     </div>
   )
